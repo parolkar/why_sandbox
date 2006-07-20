3d3d55cd6843bc772921bbb5dd77922c42c22c5b
_why <why@whytheluckystiff.net>
2006-07-20 23:34:32 +0000

    * test/test_plain.rb: proving the allowances given to the plain Sandbox by giving access to an rb_cKernel method programmatically.

--- test/test_plain.rb
+++ test/test_plain.rb
@@ -4,6 +4,10 @@ require 'sandbox'
 require 'test/unit'
 
 class TestPlain < Test::Unit::TestCase
+  def path(p)
+    File.join(File.dirname(__FILE__), p)
+  end
+
   def setup
     @s = Sandbox.new
   end
@@ -13,11 +17,14 @@ class TestPlain < Test::Unit::TestCase
   end
 
   def test_monkeypatch_the_return
-    k = eval("Kernel")
-    def k.crack_yes_this_is_allowed
-      open("/etc/passwd")
+    $TEST_PLAIN_UNSAFE_PROC = proc { open(path("files/pascal.rb")) }
+    class << eval("Kernel")
+      def crack_yes_this_is_allowed
+        $TEST_PLAIN_UNSAFE_PROC.call.read.length
+      end
     end
     assert ! Kernel.respond_to?(:crack_yes_this_is_allowed)
     assert eval("Kernel.respond_to?(:crack_yes_this_is_allowed)")
+    assert_equal 449, eval("Kernel.crack_yes_this_is_allowed")
   end
 end
